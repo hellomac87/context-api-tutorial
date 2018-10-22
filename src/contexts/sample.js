8c934d84f2979a585bb0b1c5135dfc0c3ae37e62
hellomac87 <hellomac87@gmail.com>
2018-10-22 22:12:21 +0900

    Context 만들기

---
+++ src/contexts/sample.js
@@ -0,0 +1,41 @@
+import React, { Component, createContext } from 'react';
+
+const Context = createContext(); // Context 를 만든다.
+
+// Context 안에는 Provider 와 Consumer 라는 컴포넌트가 존재한다.
+// 이 둘은, Context 를 이용하기 위해 필요한 컴포넌트들이다.
+// Consumer 는 나중에 내보낼 때 편하도록 SampleConsumer 라고 설정.
+const { Provider, Consumer : SampleConsumer } = Context;
+
+class SampleConsumer extends Component {
+    state = {
+        value: '기본값입니다.'
+    }
+
+    // actions 객체는 임의로 설정하는 객체이다.
+    // 나중에 변화를 일으키는 함수(event handler)를 전달해줄 떄, 함수 하나하나 일일이 전달하는 것이 아니라
+    // 객체 하나로 한꺼번에 전달하기 위함
+    actions = {
+        setValue : (value) => {
+            this.setState({
+                value
+            });
+        }
+    }
+
+    render() {
+        const { state, actions } = this;
+        const value = { state, actions };
+        return (
+            <Provider value={value}>
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+// 내보내줍니다.
+export {
+    SampleProvider,
+    SampleConsumer,
+};
